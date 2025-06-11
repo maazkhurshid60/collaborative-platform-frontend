fba17bef484551ef0f2a4cc01c1fb96f14618027
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-06-12 03:37:37 +0500

    UPDATE: update socket io setup with old code

--- src/socket/Socket.ts
+++ src/socket/Socket.ts
@@ -1,66 +1,25 @@
 
-// import { io, Socket } from "socket.io-client";
-// let socket: Socket | undefined;
-
-// export const initSocket = (providerId: string, userId?: string): Socket => {
-//     if (!socket || !socket.connected) {
-//         socket = io('http://localhost:8000', {
-//             query: {
-//                 providerId,
-//                 userId,
-//             },
-//             transports: ['websocket'],
-//         });
-
-//         socket.on('connect', () => {
-//             console.log('✅ Socket connected:', socket?.id);
-//         });
-
-//         socket.on('connect_error', (err) => {
-//             console.error('❌ Socket connection failed:', err.message);
-//         });
-//     }
-
-//     return socket;
-// };
-
-// export const getSocket = (): Socket | undefined => {
-//     return socket;
-// };
-
-// export const disconnectSocket = (): void => {
-//     if (socket) {
-//         socket.disconnect();
-//     }
-// };
-
-
-
-
-
 import { io, Socket } from "socket.io-client";
 let socket: Socket | undefined;
 
 export const initSocket = (providerId: string, userId?: string): Socket => {
-    if (socket) {
-        socket.disconnect();
+    if (!socket || !socket.connected) {
+        socket = io('https://collaborative-platform-backend.onrender.com', {
+            query: {
+                providerId,
+                userId,
+            },
+            transports: ['websocket'],
+        });
+
+        socket.on('connect', () => {
+            console.log('✅ Socket connected:', socket?.id);
+        });
+
+        socket.on('connect_error', (err) => {
+            console.error('❌ Socket connection failed:', err.message);
+        });
     }
-
-    socket = io('https://collaborative-platform-backend.onrender.com', {
-        query: {
-            providerId,
-            userId,
-        },
-        transports: ['websocket'],
-    });
-
-    socket.on('connect', () => {
-        console.log('✅ Socket connected:', socket?.id);
-    });
-
-    socket.on('connect_error', (err) => {
-        console.error('❌ Socket connection failed:', err.message);
-    });
 
     return socket;
 };
@@ -74,3 +33,6 @@ export const disconnectSocket = (): void => {
         socket.disconnect();
     }
 };
+
+
+
