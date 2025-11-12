4f002e69aa59f29ee41dce836e9c85678fc01fc3
Maaz Khurshid <maaz.khurshid60@gmail.com>
2025-11-13 04:29:13 +0500

    providers icon size issue resolved

--- src/components/icons/dashboardIcons/providersPortalIcons/providers/Providers.tsx
+++ src/components/icons/dashboardIcons/providersPortalIcons/providers/Providers.tsx
@@ -1,5 +1,5 @@
 const ProvidersIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg width="44" height="32" viewBox="0 0 44 32" fill="none" xmlns="http://www.w3.org/2000/svg"     {...props}>
+    <svg width="30" height="24" viewBox="0 0 44 32" fill="none" xmlns="http://www.w3.org/2000/svg"     {...props}>
         <path d="M34.5314 5.45877C32.6979 5.45877 31.2119 6.93338 31.2119 8.75276V11.0586C31.2119 12.8779 32.6979 14.3525 34.5314 14.3525C36.3649 14.3525 37.8509 12.8779 37.8509 11.0586V8.75276C37.8509 6.93338 36.3649 5.45877 34.5314 5.45877Z" stroke="currentColor" stroke-width="2.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
         <path d="M9.46891 5.45877C11.3024 5.45877 12.7884 6.93338 12.7884 8.75276V11.0586C12.7884 12.8779 11.3024 14.3525 9.46891 14.3525C7.63544 14.3525 6.14941 12.8779 6.14941 11.0586V8.75276C6.14941 6.93338 7.63544 5.45877 9.46891 5.45877Z" stroke="currentColor" stroke-width="2.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
         <path d="M22 2.00034C19.7084 2.00034 17.8506 3.84387 17.8506 6.11782V9.41181C17.8506 11.6858 19.7084 13.5293 22 13.5293C24.2915 13.5293 26.1493 11.6858 26.1493 9.41181V6.11782C26.1493 3.84387 24.2915 2.00034 22 2.00034Z" stroke="currentColor" stroke-width="2.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
